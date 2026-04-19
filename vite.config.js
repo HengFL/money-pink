970f5c48f4c82049690de668c5c477d3b33c7941
HengFL <HengFL@gmail.com>
2026-04-19 17:44:16 +0700

    chore: update base path in vite.config.js to support local development environments

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/real-estate/', // แก้ไขชื่อ repo ตรงนี้ให้ตรงกับชื่อบน GitHub
+  base: process.env.NODE_ENV === 'production' ? '/real-estate/' : '/',
   plugins: [react()],
 })
